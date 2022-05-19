a92908250a3bf064e0397dfdb560c610d7de7e25
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-05-20 00:18:34 +0900

    login and logout user url

--- base/urls.py
+++ base/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     # GET REQEUST
     path('',views.home,name='home'),
     path('room/<str:id>/',views.room,name='room'),
+    path('logout/',views.logoutUser, name= 'logout'),
 
     # POST REQUEST
     path('create-room/',views.create_room,name='create-room'),
+    path('login/',views.loginPage, name = 'login'),
 
     # PUT REQUEST 
     path('update-room/<str:pk>/', views.update_room, name = 'update-room'),
