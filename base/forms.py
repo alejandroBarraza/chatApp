4867c2eb3804fba9e5f05c4360affae04891557f
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-07-10 01:07:37 +0900

    register and login new template

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,18 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class FormRoom(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']
+       
+
+class FormUser(ModelForm):
+    class Meta:
+        model = User
+        fields = [ 'username', 'email']
+
+
 
