9b2ad57c36f25bff5f6ce3219e0af54982fcc8ce
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-07-17 02:16:45 +0900

    foreign key serializer for Room model

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,8 +1,15 @@
 from dataclasses import field
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,SlugRelatedField
 from base.models import Room
 
 class RoomSerializer(ModelSerializer):
+    participants = SlugRelatedField(
+        read_only = True,
+        many = True,
+        slug_field= 'username'
+
+    )
+
     class Meta:
         model = Room
         fields = '__all__'
